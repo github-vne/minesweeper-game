a4750978b1d69ed1c3002022bede4e9ec81baf2a
vasen <vasenking@mail.ru>
2019-03-22 13:28:54 +0500

    Settings Component's

--- client/src/Components/Main/Settings/index.js
+++ client/src/Components/Main/Settings/index.js
@@ -5,7 +5,18 @@ class Settings extends Component {
     render() {
         return (
             <section className="game_settings">
-
+                <h4>Настройки</h4>
+                <div className="settings_box">
+                    <label>Размер поля</label>
+                    <input type="number"/>
+                </div>
+                <div className="settings_box">
+                    <label>Кол-во мин</label>
+                    <input type="number"/>
+                </div>
+                <div className="apply_settings">
+                    <button className="gold_btn">Применить</button>
+                </div>
             </section>
         )
     }
